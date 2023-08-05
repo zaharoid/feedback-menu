380ef58ab81cf9dc9d8f2ba17c4cd0af00e327be
zaharoid <gololobov.zahar23@gmail.com>
2023-08-05 21:33:38 +0200

    /

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,46 +1,56 @@
-import { React, Component } from 'react';
+import { useState } from 'react';
 import Statistics from '../Statistics';
 import FeedbackOptions from '../FeedbackOptions';
 import Section from '../Section';
 import { Container } from 'components/Section/Section.styled';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  onBtnClick = e => {
+  function onBtnClick(e) {
     const name = e.target.value;
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
-  };
 
-  countTotalFeedback() {
-    return Object.values(this.state).reduce((acc, value) => (acc += value), 0);
+    switch (name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   }
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const buttons = Object.keys(this.state);
+  const buttons = [good, neutral, bad];
 
-    return (
-      <>
-        <Section title={'Please leave feedback'}>
-          <Container>
-            <FeedbackOptions
-              options={buttons}
-              onLeaveFeedback={this.onBtnClick}
-            />
-            <Statistics good={good} neutral={neutral} bad={bad} total={total} />
-          </Container>
-        </Section>
-      </>
-    );
+  function countTotalFeedback() {
+    return buttons.reduce((acc, value) => (acc += value), 0);
   }
+
+  return (
+    <>
+      <Section title={'Please leave feedback'}>
+        <Container>
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={onBtnClick}
+          />
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+          />
+        </Container>
+      </Section>
+    </>
+  );
 }
 
 export default App;
